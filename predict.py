7f0954b90949dd9a8a8e3a87c65122666c23279a
Dhruv Kimbahune <dhruvvk05@gmail.com>
2026-03-05 10:28:52 +0530

    done with model starting mlops

---
+++ predict.py
@@ -0,0 +1,48 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input
+
+
+
+MODEL_PATH = "models/final_model.keras"
+IMG_SIZE = 224
+
+
+CLASS_NAMES = ['NORMAL', 'bacterial', 'viral']
+
+print("Loading model...")
+model = tf.keras.models.load_model(MODEL_PATH)
+print("Model loaded successfully!")
+
+
+def predict_image(img_path):
+
+    
+    img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
+
+    img_array = image.img_to_array(img)
+
+    img_array = np.expand_dims(img_array, axis=0)
+
+    img_array = preprocess_input(img_array)
+
+    prediction = model.predict(img_array)
+
+    predicted_class = CLASS_NAMES[np.argmax(prediction)]
+    confidence = np.max(prediction) * 100
+
+    print("\nPrediction:", predicted_class)
+    print("Confidence: {:.2f}%".format(confidence))
+
+if __name__ == "__main__":
+
+    img_path = input("Enter path of chest X-ray image: ")
+
+    if os.path.exists(img_path):
+        predict_image(img_path)
+    else:
+        print("Image path not found!")
---
+++ train.py
@@ -0,0 +1,134 @@
+import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras import layers, models
+
+IMG_SIZE = 224
+BATCH_SIZE = 16
+EPOCHS = 10
+
+TRAIN_DIR = "chest_xray/train"
+VAL_DIR = "chest_xray/val"
+TEST_DIR = "chest_xray/test"
+
+train_datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_input,
+    rotation_range=15,
+    zoom_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True
+)
+
+val_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+train_gen = train_datagen.flow_from_directory(
+    TRAIN_DIR,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="categorical"
+)
+
+val_gen = val_datagen.flow_from_directory(
+    VAL_DIR,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="categorical"
+)
+
+test_gen = test_datagen.flow_from_directory(
+    TEST_DIR,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    shuffle=False
+)
+
+NUM_CLASSES = len(train_gen.class_indices)
+
+print("Classes detected:", train_gen.class_indices)
+
+base_model = EfficientNetB0(
+    weights="imagenet",
+    include_top=False,
+    input_shape=(IMG_SIZE, IMG_SIZE, 3)
+)
+
+base_model.trainable = False
+
+model = models.Sequential([
+    base_model,
+    layers.GlobalAveragePooling2D(),
+    layers.BatchNormalization(),
+    layers.Dense(256, activation="relu"),
+    layers.Dropout(0.4),
+    layers.Dense(NUM_CLASSES, activation="softmax")
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.summary()
+
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss",
+        patience=5,
+        restore_best_weights=True
+    ),
+        tf.keras.callbacks.ModelCheckpoint(
+        "models/best_model.keras",
+        save_best_only=True
+    )
+]
+class_weight = {
+    0: 1.0,   
+    1: 0.8,   
+    2: 1.0    
+}
+history = model.fit(
+    train_gen,
+    validation_data=val_gen,
+    epochs=EPOCHS,
+    callbacks=callbacks,
+    class_weight=class_weight
+)
+print("Starting Fine-Tuning...")
+
+
+model.load_weights("models/best_model.keras")
+
+base_model.trainable = True
+
+
+for layer in base_model.layers[:-30]:
+    layer.trainable = False
+
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(1e-5),
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+history_fine = model.fit(
+    train_gen,
+    validation_data=val_gen,
+    epochs=10,
+    class_weight=class_weight
+)
+
+loss, acc = model.evaluate(test_gen)
+
+print(f"Test Accuracy: {acc*100:.2f}%")
+
+model.save("models/final_model.keras")
